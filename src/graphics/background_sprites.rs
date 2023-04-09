a5c13a97bcbfe3e31f31196b5a2a575dc3968802
yopox <yopoxdev@gmail.com>
2023-04-09 19:13:09 +0200

    background: Add background_sprites.rs

---
+++ src/graphics/background_sprites.rs
@@ -0,0 +1,57 @@
+use rand::prelude::SliceRandom;
+use strum_macros::EnumIter;
+
+use crate::graphics::sprites::TILE;
+
+pub enum ElementSize {
+    Small,
+    Medium,
+    Large,
+}
+
+impl ElementSize {
+    pub fn get_sprite(&self) -> &[TILE] {
+        match self {
+            ElementSize::Small => [THING].choose(&mut rand::thread_rng()),
+            ElementSize::Medium => [THING].choose(&mut rand::thread_rng()),
+            ElementSize::Large => [THING].choose(&mut rand::thread_rng()),
+        }.unwrap()
+    }
+}
+
+#[derive(EnumIter)]
+pub enum Layouts {
+    Layout1,
+    Layout2,
+}
+
+impl Layouts {
+    fn get_layout(&self) -> Vec<(ElementSize, usize, usize)> {
+        match self {
+            Layouts::Layout1 => vec![
+                (ElementSize::Small, 1, 3),
+                (ElementSize::Medium, 4, 2),
+            ],
+            Layouts::Layout2 => vec![
+                (ElementSize::Large, 2, 2),
+            ]
+        }
+    }
+}
+
+const NO_NAME: [TILE; 14] = [
+    (0, 1, 455, 0, 3, true, 1),
+    (1, 1, 206, 0, 3, true, 2),
+    (2, 1, 231, 0, 3, false, 3),
+    (3, 1, 302, 0, 3, false, 1),
+    (4, 1, 231, 0, 3, true, 3),
+    (5, 1, 206, 0, 3, true, 2),
+    (6, 1, 455, 0, 3, false, 1),
+    (0, 0, 455, 0, 3, false, 3),
+    (1, 0, 206, 0, 3, true, 0),
+    (2, 0, 231, 0, 3, false, 0),
+    (3, 0, 129, 0, 3, false, 2),
+    (4, 0, 231, 0, 3, false, 0),
+    (5, 0, 271, 0, 3, false, 3),
+    (6, 0, 455, 0, 3, true, 3),
+];
